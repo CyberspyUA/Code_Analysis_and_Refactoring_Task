ae7be83d7eda8dd2b4a649d8e82d938276ca592d
Nikita Struk <88832410+CyberspyUA@users.noreply.github.com>
2023-10-02 17:42:00 +0300

    Lab Task 4 added.
    Project "Lab task 4" added.
    Refactoring in progress.

---
+++ Lab_Task_4/C#/Struk_Nikita_CAR_01/Struk_Nikita_CAR_01/Coffee.cs
@@ -0,0 +1,34 @@
+﻿namespace Product
+{
+    using System;
+
+    internal class Coffee : Drink
+    {
+
+        public Coffee(string drinkName_, int cupSize_, int sugarTeaSpoonQuantity_) 
+            : base(drinkName_, cupSize_, sugarTeaSpoonQuantity_)
+        {
+        }
+        
+        /**
+         * Метод, що готує каву.
+         */
+        public override void Brewing()
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("Грiємо молоко та воду.");
+            Thread.Sleep(3000);
+            Console.WriteLine("Мелимо кавовi зерна.");
+            Thread.Sleep(3000);
+            Console.WriteLine("Готуємо стакан.");
+            Thread.Sleep(3000);
+            Console.WriteLine("Наливаємо каву у стакан.");
+            SugarAdding();
+            Console.WriteLine("Ваш " + GetDrinkName() + " готовий! Смачного!");
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+        
+
+        
+    }
+}
