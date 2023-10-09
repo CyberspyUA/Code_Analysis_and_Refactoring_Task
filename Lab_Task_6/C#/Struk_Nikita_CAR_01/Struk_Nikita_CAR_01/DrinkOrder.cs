857844343194abbae6c83cc002e1ea5f69f7a7ed
Nikita Struk <88832410+CyberspyUA@users.noreply.github.com>
2023-10-09 22:51:21 +0300

    Major changes
    Lab Task 6 added.

---
+++ Lab_Task_6/C#/Struk_Nikita_CAR_01/Struk_Nikita_CAR_01/DrinkOrder.cs
@@ -0,0 +1,33 @@
+﻿using Product;
+namespace UI
+{
+    internal static class DrinkOrder
+    {
+        public static void DisplayOrderAndBrewDrink(int drinkTypeChoice, string drinkName, int cupSize, int sugarSpoons)
+        {
+            Drink drink;
+
+            switch (drinkTypeChoice)
+            {
+                case 1:
+                    drink = new Coffee(drinkName, cupSize, sugarSpoons);
+                    break;
+                case 2:
+                    drink = new Tea(drinkName, cupSize, sugarSpoons);
+                    break;
+                case 3:
+                    drink = new Cacao(drinkName, cupSize, sugarSpoons);
+                    break;
+                default:
+                    Console.WriteLine("Вибачте, ця варiант є недоступним. Спробуйте ще раз.");
+                    return;
+            }
+
+            Console.WriteLine("Ваше замовлення");
+            Console.WriteLine("-----------------------------------------------------------------");
+            drink.OrderShow();
+            Console.WriteLine("Очiкуйте на завершення приготування " + drinkName);
+            drink.Brewing();
+        }
+    }
+}
